ebde41052446476ea183da68621e86f0211caaf1
Weslley F. Souza <weslley.fernandes@aluno.ufca.edu.br>
2026-09-14 23:38:48 -0300

    feat: adiciona init das dependências

--- firmware/main/main.cpp
+++ firmware/main/main.cpp
@@ -1,19 +1,18 @@
 #include <cstdio>
 
-#include <ds3231.h>
+#include <ds3231.hpp>
 #include <i2c.hpp>
-#include <inmp441.h>
+#include <inmp441.hpp>
 #include <mqttw.hpp>
 #include <sdcard.hpp>
 #include <wifi.hpp>
 
 extern "C" void app_main(void) {
-	uint16_t sample_rate = 16000, buffer_size = 512;
-
-	i2c::master::init();
-	ds3231_init();
-	sdcard::init(SDCARD_API_SDSPI);
-	inmp441_init(sample_rate, buffer_size);
-	wifi::init();
-	mqttw::start();
+	ESP_ERROR_CHECK(i2c::master::init());
+	ESP_ERROR_CHECK(nvs::init());
+	ESP_ERROR_CHECK(ds3231::init());
+	ESP_ERROR_CHECK(sdcard::init());
+	ESP_ERROR_CHECK(inmp441::init());
+	ESP_ERROR_CHECK(wifi::init());
+	ESP_ERROR_CHECK(mqttw::start());
 }
